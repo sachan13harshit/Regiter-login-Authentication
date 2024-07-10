052b3d7a1e57230f332cdaf71b9381dfb366e829
Harshit Sachan <sachanharshit80@gmail.com>
2024-07-10 10:38:57 +0530

    first commit

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,7 +4,23 @@ const User = require("../models/userModel");
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
+    try{
+        const userExists = await User.findOne({email:req.body.email});
 
+        if(userExists){
+            return res.send({
+                success : false,
+                message : "User Already exists"
+            })
+        }
+        const newUser = new User(req.body);
+        await newUser.save();
+
+        return res.status(201).json('User Created');
+    }
+    catch(error){
+        res.json(error);
+    }
 
 });
 
